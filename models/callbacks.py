d4116c95bd2174785c41a8df0d38f20a8f8e8fd4
Engh <dor@ssb.no>
2022-04-08 15:05:24 +0200

    Burde fungere bedre nå

--- models/callbacks.py
+++ models/callbacks.py
@@ -1,4 +1,49 @@
+import sqlite3
+from sqlalchemy import Table, create_engine
+from sqlalchemy.sql import select
+from flask_sqlalchemy import SQLAlchemy
+import cx_Oracle
+import pandas as pd
+import dash
+
+from models.models_delt import connect
+from models.models_homepage import svarinngang_linje, svarinngang_kake, svarinngang_tbl1, klargjor_tbl1_svar
+from models.models_grid import treeplot, table_grid, scatterplot_grid, histogram_grid, boxplot_grid, sammenlign_editert_ueditert
+#from models.models_plots import bubble_plt_side
+from models.models_enhet import enhetstabell1, enhet_plot, enhetstabell_store, update_columns, enhet_plot_bar_agg, offcanvas_innhold
+#from models.models_tidsserie import display_time_series
+from models.models_logg import logg_tabell
+from models.models_kontroller import feilliste_tabell, innhent_feilliste, oppdater_feilliste_db, model_feilliste_figur, kontroll_enhetstabell_store, kontroll_update_columns, kontroll_enhetstabell, kontroll_offcanvas_innhold
+
+from templates.homepage import Svarinngang
+from templates.navbar import Navbar
+#from templates.tidsserie import Tidsserie
+from templates.grid import Grid
+#from templates.vektet import Plots
+from templates.enhet import Enhet
+from templates.logg import Logg
+from templates.kontroller import Kontroller
+
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_table as dt
+import dash_cytoscape as cyto
+import dash_pivottable as dpt
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import dash_auth
+import flask
+
+from flask import request # for brukernavn
+
+from datetime import datetime
+from datetime import timedelta
+
+import json
 
 def get_callbacks(app):
     ##############################################
